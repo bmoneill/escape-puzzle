ab111105e2d7e4369b26ba29041ad9605ab77e95
NETOPS_STUDENT2 <NETOPS_STUDENT2@wit.edu>
2026-05-27 09:12:08 -0400

    Test commit

--- src/escapepuzzle.c
+++ src/escapepuzzle.c
@@ -10,15 +10,10 @@
 #include <stdio.h>
 
 int start_game(void) {
-<<<<<<< HEAD
     printf("Welcome to the Escape Puzzle Game!\n");
     printf("Test commit ignore");
-=======
     ui_init(&(UI) { 0 });
->>>>>>> 8d41d9c65597102b9646521c04723c06dffd9672
     return 0;
 }
 
 const char* escapepuzzle_version(void) { return ESCAPEPUZZLE_VERSION; }
-
-printf("Test commit ignore");
