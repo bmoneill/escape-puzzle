256e8dad6949a913498ee98dcdf0968071cc1de2
quangdatwit <113364027+quangdatwit@users.noreply.github.com>
2026-05-27 13:06:19 +0000

    Deploying to gh-pages from @ bmoneill/escape-puzzle@caa678744798bc2d2cb7352bf01cd57a09c467b5 🚀

--- escapepuzzle_8c.js
+++ escapepuzzle_8c.js
@@ -1,6 +1,5 @@
 var escapepuzzle_8c =
 [
     [ "escapepuzzle_version", "escapepuzzle_8c.html#ad24be0a0927b5f966a5dab885a972a4a", null ],
-    [ "printf", "escapepuzzle_8c.html#a05bbde7c43a829876e4e4998e99e5188", null ],
     [ "start_game", "escapepuzzle_8c.html#a7b3c5eabdee23a068174748b1d2df871", null ]
 ];
--- search/all_c.js
+++ search/all_c.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['peak_0',['peak',['../structMemoryManager.html#a98f434f34d860b14fd6d903b7bec9759',1,'MemoryManager']]],
-  ['printf_1',['printf',['../escapepuzzle_8c.html#a05bbde7c43a829876e4e4998e99e5188',1,'escapepuzzle.c']]]
+  ['peak_0',['peak',['../structMemoryManager.html#a98f434f34d860b14fd6d903b7bec9759',1,'MemoryManager']]]
 ];
