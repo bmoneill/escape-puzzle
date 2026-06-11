d2e427ab22a079adc4ae001efce127a9720797f4
Ben O'Neill <ben@oneill.sh>
2026-06-11 13:50:16 -0400

    Add test case skeletons for map generate

---
+++ test/game/test_map.c
@@ -0,0 +1,25 @@
+#include "game/map.h"
+
+#include "unity.h"
+
+void test_map_generate_DoesNotPlacePuzzlesOnWalls(void) {}
+
+void test_map_generate_DoesNotPlacePuzzlesOnPlayerStart(void) {
+    // TODO
+}
+
+void test_map_generate_DoesNotPlaceWallsOnPlayerStart(void) {
+    // TODO
+}
+
+void test_map_generate_DoesNotGeneratePuzzlesOutOfBounds(void) {
+    // TODO
+}
+
+void test_map_generate_DoesNotGeneratePuzzlesInSameLocation(void) {
+    // TODO
+}
+
+void test_map_generate_DoesNotGeneratePuzzlesInInaccessibleAreas(void) {
+    // TODO
+}
