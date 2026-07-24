49bf93e67b2b8f89c8d814a742931c1d16bca3f8
NETOPS_STUDENT2 <NETOPS_STUDENT2@wit.edu>
2026-07-24 07:52:15 -0400

    Updated menu ui and added name

--- src/graphics/raylib/menu.c
+++ src/graphics/raylib/menu.c
@@ -17,16 +17,78 @@ void        menu_init(MenuItem* items, i32 num_items) {
             CloseWindow();
             return;
         }
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+       BeginDrawing();
 
-        for (i32 i = 0; i < num_items; i++) {
-            Color color = (i == selected) ? RED : BLACK;
-            DrawText(items[i].name, 100, 100 + i * 40, 20, color);
-        }
+ClearBackground((Color){18, 18, 30, 255});
 
-        EndDrawing();
+// Title
+DrawText("PUZZLE REALM",
+         GetScreenWidth()/2 - MeasureText("PUZZLE REALM", 48)/2,
+         50,
+         48,
+         GOLD);
+
+DrawText("Solve the puzzles. Escape the realm.",
+         GetScreenWidth()/2 - MeasureText("Solve the puzzles. Escape the realm.", 20)/2,
+         105,
+         20,
+         LIGHTGRAY);
+
+// Menu box
+int boxX = GetScreenWidth()/2 - 220;
+int boxY = 170;
+int boxW = 440;
+int boxH = num_items * 55 + 30;
+
+DrawRectangle(boxX, boxY, boxW, boxH, (Color){35,35,45,255});
+DrawRectangleLines(boxX, boxY, boxW, boxH, GRAY);
+
+// Menu items
+for (i32 i = 0; i < num_items; i++) {
+
+    int y = boxY + 20 + i * 55;
+
+    if (i == selected) {
+
+        DrawRectangle(boxX + 10, y - 5, boxW - 20, 40, DARKBLUE);
+
+        DrawText(">",
+                 boxX + 25,
+                 y,
+                 28,
+                 YELLOW);
+
+        DrawText(items[i].name,
+                 boxX + 60,
+                 y,
+                 28,
+                 YELLOW);
+
+    } else {
+
+        DrawText(items[i].name,
+                 boxX + 60,
+                 y,
+                 28,
+                 RAYWHITE);
     }
+}
+
+// Controls
+DrawText("UP/DOWN  Navigate",
+         GetScreenWidth()/2 - MeasureText("UP/DOWN  Navigate",18)/2,
+         GetScreenHeight()-80,
+         18,
+         LIGHTGRAY);
+
+DrawText("ENTER  Select    ESC  Quit",
+         GetScreenWidth()/2 - MeasureText("ENTER  Select    ESC  Quit",18)/2,
+         GetScreenHeight()-50,
+         18,
+         LIGHTGRAY);
+
+EndDrawing();
+}
 }
 
 static bool menu_update(MenuItem* items, i32 num_items, i32* selected_item) {
